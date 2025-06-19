2bb9cfa05e4ab7cf6fe3be82bf835a995c906c54
RodriguezAlania <166547004+RodriguezAlania@users.noreply.github.com>
2025-06-19 12:10:42 -0500

    Obtención de datos

--- CodigoFuente_ABR.cpp
+++ CodigoFuente_ABR.cpp
@@ -12,6 +12,19 @@ struct Nodo {
     Nodo* derecha;
 };
 
+Nodo(int _ID, string _nombre, string _nac, string _def, char _sexo, string _rol) {
+        ID = _ID;
+        nombre = _nombre;
+        fechaNacimiento = _nac;
+        fechaDefuncion = _def;
+        sexo = _sexo;
+        rol = _rol;
+        izquierda = NULL;
+        derecha = NULL;
+    }
+};
+
+
 Nodo* insertar(Nodo* raiz, Nodo* nuevo) {
     if (raiz == nullptr)
         return nuevo;
